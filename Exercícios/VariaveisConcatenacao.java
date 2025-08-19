0157af5c1fd7384b1c0040f0bc256cf13fe4b6e6
Caio Seabra de Queiroz <144072410+caioxdev@users.noreply.github.com>
2025-08-18 22:17:42 -0300

    Exercícios de fixação e aprofundamento

---
+++ Exercícios/VariaveisConcatenacao.java
@@ -0,0 +1,8 @@
+public class VariaveisConcatenacao {
+    public static void main(String[] args) {
+        String nome = "João";
+        int idade = 16;
+
+        System.out.println("Meu nome é " + nome + " e tenho " + idade + " anos");
+    }
+}
