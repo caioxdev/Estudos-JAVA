17fab98db9443ad69961c604ba60e10a77fb914d
Caio Seabra de Queiroz <144072410+caioxdev@users.noreply.github.com>
2025-08-19 16:54:49 -0300

    Tabuada simples com if-else

---
+++ Exercícios/TabuadaIfElse.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class TabuadaIfElse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int som, subt, mult, div;
+
+        System.out.print("Selecione a operação que você que fazer (+, -, * e /: ");
+        String opera = sc.nextLine();
+        System.out.println();
+
+        System.out.print("Digite o primeiro número: ");
+        int n1 = sc.nextInt();
+
+        System.out.print("Digite o segundo número: ");
+        int n2 = sc.nextInt();
+
+        if (opera.equals("+")) {
+            som = n1 + n2;
+            System.out.println();
+            System.out.println("A soma entre os números é: " + som);
+        } else if (opera.equals("-")) {
+            subt = n1 - n2;
+            System.out.println();
+            System.out.println("A subtração ente os números é: " + subt);
+        } else if (opera.equals("*")) {
+            mult = n1 * n2;
+            System.out.println();
+            System.out.println("A multiplicação entre os números é: " + mult);
+        } else if (opera.equals("/")) {
+            div = n1 / n2;
+            System.out.println();
+            System.out.println("A divisão entre os números é: " + div);
+        }
+        sc.close();
+    }
+}
